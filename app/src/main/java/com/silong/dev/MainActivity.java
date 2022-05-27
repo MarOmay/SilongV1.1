ea8eba54e4523bfbe8276531a53362a3be670606
MarOmay <maralexisomay@gmail.com>
2022-05-27 20:45:06 +0800

    Initial commit

---
+++ app/src/main/java/com/silong/dev/MainActivity.java
@@ -0,0 +1,24 @@
+package com.silong.dev;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.yalantis.library.Koloda;
+import com.yalantis.library.KolodaListener;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
