98e72e66653a438fb52e7a6b18db0362a411c164
charleology <cnbelano@gmail.com>
2022-07-22 16:49:12 +0800

    Filter ui and Koloda Swipecards.

---
+++ app/src/main/java/com/silong/dev/Header.java
@@ -0,0 +1,15 @@
+package com.silong.dev;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Header extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_header);
+        getSupportActionBar().hide();
+    }
+}
--- app/src/main/java/com/silong/dev/Homepage.java
+++ app/src/main/java/com/silong/dev/Homepage.java
@@ -1,9 +1,22 @@
 package com.silong.dev;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.text.Html;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.google.android.material.dialog.MaterialAlertDialogBuilder;
+import com.yalantis.library.Koloda;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Homepage extends AppCompatActivity {
 
@@ -17,12 +30,61 @@ public class Homepage extends AppCompatActivity {
     protected final File PETDATA = new File(getFilesDir(),"pet.dat");
     protected final File FAVORITECONFIG = new File(getFilesDir(),"favorite.config");
 
+    private SwipeAdapter adapter;
+    private List<Integer> list;
+    Koloda koloda;
+
+    TextView headerTitle;
+    ImageView filterImgview, messageImgview, menuImgview;
+    Button applyBtn;
+    ImageView heartIcon;
+
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_homepage);
-        
+        getSupportActionBar().hide();
+
+        View view = findViewById(R.id.headerLayout);
+        headerTitle = (TextView) findViewById(R.id.headerTitle);
+        filterImgview = (ImageView) findViewById(R.id.filterImgview);
+        messageImgview = (ImageView) findViewById(R.id.messageImgview);
+        menuImgview = (ImageView) findViewById(R.id.menuImgview);
+        applyBtn = (Button) findViewById(R.id.applyBtn);
+        heartIcon = (ImageView) findViewById(R.id.heartIcon);
+        filterImgview.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                filterDia(Homepage.this);
+            }
+        });
+
+        //Koloda swipe
+        koloda = findViewById(R.id.koloda);
+        list = new ArrayList<>();
+
+        adapter = new SwipeAdapter(this, list);
+        koloda.setAdapter(adapter);
+    }
+    //Method for executing Filter Dialog
+    public void filterDia(Context context){
+        MaterialAlertDialogBuilder builder = new MaterialAlertDialogBuilder(context);
+        builder.setBackground(getDrawable(R.drawable.dialog_bg));
+        builder.setView(R.layout.filter_layout);
+        builder.setPositiveButton(Html.fromHtml("<b>"+"APPLY"+"</b>"), new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                //Codes here
+            }
+        });
+        builder.setNegativeButton(Html.fromHtml("<b>"+"CANCEL"+"</b>"), new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                //codes here
+            }
+        });
+        builder.show();
     }
 }
