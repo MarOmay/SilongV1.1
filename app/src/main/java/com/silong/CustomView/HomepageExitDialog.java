79f3b6216d51d0fd6dc55aebf4c7e4494e6ce238
charleology <cnbelano@gmail.com>
2022-08-12 16:43:23 +0800

    Homepage Exit Dialog added.

---
+++ app/src/main/java/com/silong/CustomView/HomepageExitDialog.java
@@ -0,0 +1,35 @@
+package com.silong.CustomView;
+
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.text.Html;
+
+import androidx.annotation.NonNull;
+import androidx.localbroadcastmanager.content.LocalBroadcastManager;
+
+import com.google.android.material.dialog.MaterialAlertDialogBuilder;
+import com.silong.dev.R;
+
+public class HomepageExitDialog extends MaterialAlertDialogBuilder {
+
+    public HomepageExitDialog(@NonNull Context context) {
+        super(context);
+        super.setTitle(Html.fromHtml("<b>"+"Exit"+"</b>"));
+        super.setIcon(context.getDrawable(R.drawable.circlelogo_gradient));
+        super.setBackground(context.getDrawable(R.drawable.dialog_bg));
+        super.setMessage("Do you want to exit the app?\n");
+        super.setPositiveButton(Html.fromHtml("<b>"+"YES"+"</b>"), new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                //codes here
+            }
+        });
+        super.setNegativeButton(Html.fromHtml("<b>"+"NO"+"</b>"), new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                //codes here
+            }
+        });
+    }
+}
